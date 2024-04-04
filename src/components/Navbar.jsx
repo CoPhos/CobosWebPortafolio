cfe5898a0595528c2e8fe579c1af0c945e91b626
Jorge Cobos <cobos1932@gmail.com>
2024-04-03 18:11:51 -0600

    add support for different languages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,43 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 function Navbar() {
-  return (
-      <div className="w-full bg-slate-500 h-[160px] flex flex-row items-center justify-around ">
-          <div className="">Jorge Cobos</div>
-          <ul className="flex flex-row items-center justify-between gap-6">
-              <li>
-                  <a href="">About</a>
-              </li>
-              <li>
-                  <a href="">Experience</a>
-              </li>
-              <li>
-                  <a href="">Projects</a>
-              </li>
-              <li>
-                  <a href="">Contact</a>
-              </li>
-          </ul>
-      </div>
-  )
+    const [tranlation, i18n] = useTranslation('global')
+    const handleChangeLanguage = (language) => {
+        i18n.changeLanguage(language)
+    }
+    return (
+        <div className="w-full  h-[160px] flex flex-row items-center justify-around ">
+            <div className="">{tranlation('navbar.1')}</div>
+            <div className="flex flex-row items-center justify-start gap-1">
+                <ul className="flex flex-row items-center justify-between gap-6">
+                    <li>
+                        <a href="">{tranlation('navbar.2')}</a>
+                    </li>
+                    <li>
+                        <a href="">{tranlation('navbar.3')}</a>
+                    </li>
+                    <li>
+                        <a href="">{tranlation('navbar.4')}</a>
+                    </li>
+                    <li>
+                        <a href="">{tranlation('navbar.5')}</a>
+                    </li>
+                </ul>
+                <div className='h-[16px] w-[2px] bg-black mx-1'></div>
+                <select onChange={(event) => handleChangeLanguage(event.target.value)}>
+                    <option value="en">{tranlation('language.1')}</option>
+                    <option value="es">{tranlation('language.2')}</option>
+                </select>
+            </div>
+        </div>
+
+        //       -webkit-appearance: none;
+        // -moz-appearance: none;
+        // -ms-appearance: none;
+        // -o-appearance: none;
+        // appearance: none;
+    )
 }
 
 export default Navbar
--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import global_en from '../src/assests/translations/en/global.json'
+import global_es from '../src/assests/translations/es/global.json'
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng: 'en',
+    resources: {
+        en: {
+            global: global_en,
+        },
+        es: {
+            global: global_es,
+        },
+    },
+})
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <I18nextProvider i18n={i18next}>
+            <App />
+        </I18nextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 )
