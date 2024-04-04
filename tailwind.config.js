5e22d1c2d660d92b95f6f044ff36ff7ddf54c997
Jorge Cobos <cobos1932@gmail.com>
2024-04-04 17:56:29 -0600

    add font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,48 @@ module.exports = {
             sans: ['Poppins', 'sans-serif'],
             'sans-regular': ['Poppins-Regular', 'sans-serif'],
         },
+        fontSize: {
+            'base': [
+                '1rem',
+                {
+                    lineHeight: '1.5rem',
+                    letterSpacing: '-0.04em',
+                    fontWeight: '500',
+                },
+            ],
+            'xl': [
+                '1.5rem',
+                {
+                    lineHeight: '2rem',
+                    letterSpacing: '-0.01em',
+                    fontWeight: '500',
+                },
+            ],
+            '2xl': [
+                '1.75rem',
+                {
+                    lineHeight: '2.2rem',
+                    letterSpacing: '-0.017em',
+                    fontWeight: '700',
+                },
+            ],
+            '3xl': [
+                '2rem',
+                {
+                    lineHeight: '2.3rem',
+                    letterSpacing: '-0.02em',
+                    fontWeight: '400',
+                },
+            ],
+            '4xl': [
+                '3rem',
+                {
+                    lineHeight: '3.5rem',
+                    letterSpacing: '-0.03em',
+                    fontWeight: '700',
+                },
+            ],
+        },
     },
     plugins: [],
 }
